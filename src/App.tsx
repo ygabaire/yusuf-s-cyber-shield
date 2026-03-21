aa84173597f645e2ffc610834209ae66ccd30d51
ygabaire <85384697+ygabaire@users.noreply.github.com>
2026-03-21 02:21:07 -0400

    Fix BrowserRouter JSX syntax error

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRoute basename="/portfolio/">>
-        <Routes>
+      <BrowserRouter basename="/portfolio/">        <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
